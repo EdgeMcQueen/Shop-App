fbab887d042ba0c85c932c53620456ae14a229af
sardormutalov <sardor.mutalov@gmail.com>
2020-09-02 07:43:51 +0300

    init project

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+// default
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+// Pages
+import Home from '@/pages/Home'
+import NotFound from '@/pages/404'
+import Shop from '@/pages/Shop'
+
+// Routering
+export default new Router({
+    // mode: 'history', //контроль ставить ли хэш или нет
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '*',
+            name: 'notFound',
+            component: NotFound
+        },
+        {
+            path: '/shop',
+            name: 'shop',
+            component: Shop
+        },
+    ]
+})
