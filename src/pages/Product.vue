1b0177a1ad2b5ec8d059a4d083e8650dbea0a972
sardormutalov <sardor.mutalov@gmail.com>
2020-09-05 15:44:03 +0300

    ShopApp: product page

---
+++ src/pages/Product.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <h1>Product: </h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log({route: this.$route, id: this.$route.params.id})
+    },
+}
+</script>
